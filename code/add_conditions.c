89885c2cd8276b6d3f59254da671cc3cda3def97
katorec <kekemeciarova@gmail.com>
2024-02-23 09:51:17 +0100

    feat: tshbatts - second try implement

--- code/add_conditions.c
+++ code/add_conditions.c
@@ -50,12 +50,11 @@ void at_most_one_street_for_each_step(CNF* formula, unsigned num_of_crossroads, 
 
     // pre kazdy krok
     for (unsigned i=0; i<num_of_streets; ++i) {
-
         // kazdej ulice
         for (unsigned  j=0; j<num_of_streets; ++j) {
-                //kazdeho konca1
-                for (unsigned k1=0; k1<num_of_crossroads; ++k1) {
 
+            //kazdeho konca1
+                for (unsigned k1=0; k1<num_of_crossroads; ++k1) {
                     //kazdeho konca2
                     for (unsigned k2 = 0; k2 < num_of_crossroads; ++k2) {
 
@@ -64,7 +63,7 @@ void at_most_one_street_for_each_step(CNF* formula, unsigned num_of_crossroads, 
                             //kazdeho zaciatku2
                             for (unsigned z2 = 0; z2 < num_of_crossroads; ++z2) {
 
-                                if (z1!=z2 && k1!=k2 && z1!=k1 && z2!=k2 && i!=j) {
+                                if ((z1!=z2 || k1!=k2) && (z1!=k1 || z2!=k2) && i!=j) {
                                     Clause *cl = create_new_clause(formula);
                                     add_literal_to_clause(cl, false, i, z1, k1);
                                     add_literal_to_clause(cl, false, j, z2, k2);
@@ -94,22 +93,22 @@ void streets_connected(CNF* formula, unsigned num_of_crossroads, unsigned num_of
 
         unsigned j=i+1;
 
-                //kazdeho konca
-                for (unsigned k=0; k<num_of_crossroads; ++k) {
+        //kazdeho konca1
+        for (unsigned k1=0; k1<num_of_crossroads; ++k1) {
+            //kazdeho konca2
+            for (unsigned k2 = 0; k2 < num_of_crossroads; ++k2) {
 
-                    //kazdeho zaciatku
-                    for (unsigned z=0; z<num_of_crossroads; ++z) {
-                        for (unsigned z1=0; z1<num_of_crossroads; ++z1) {
-                            for (unsigned k1=0; k1<num_of_crossroads; ++k1) {
+                //kazdeho zaciatku1
+                for (unsigned z1 = 0; z1 < num_of_crossroads; ++z1) {
+                    //kazdeho zaciatku2
+                    for (unsigned z2 = 0; z2 < num_of_crossroads; ++z2) {
 
-                        // existuje zaciatok nejakej ulice zhodny s koncom inej ulice
-
-                        if(k!=z1 || k1!=z ) {
-
-                            /*klauzula*/
+//  !!!!!                        //mas podmienku, kt ti to neprejde loopmi, zato to nejde (aj hore)
+                        if ((z1!=z2 || k1!=k2) && (z1!=k1 || z2!=k2) && i!=j && k1==z2) {
                             Clause *cl = create_new_clause(formula);
-                            add_literal_to_clause(cl, false, i, z1, k);
-                            add_literal_to_clause(cl, true, j, z, k1);
+                            add_literal_to_clause(cl, false, i, z1, k1);
+                            add_literal_to_clause(cl, false, j, z2, k2);
+
                         }
                     }
                 }
@@ -117,6 +116,7 @@ void streets_connected(CNF* formula, unsigned num_of_crossroads, unsigned num_of
         }
     }
 }
+
 
 // Tato funkce by mela do formule pridat klauzule predstavujici podminku 4)
 // Křižovatky jsou reprezentovany cisly 0, 1, ..., num_of_crossroads-1
