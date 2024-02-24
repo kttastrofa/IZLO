1232c8eca90a18808c8e1a2d6898e1fb4855eb9a
katorec <kekemeciarova@gmail.com>
2024-02-24 15:05:27 +0100

    feat: help arrived

--- code/add_conditions.c
+++ code/add_conditions.c
@@ -19,10 +19,16 @@ void at_least_one_valid_street_for_each_step(CNF* formula, unsigned num_of_cross
 
     // pre kazdy krok i
     for (unsigned i=0; i<num_of_streets; ++i) {
-        Clause *cl = create_new_clause(formula);
-        add_literal_to_clause(cl, true, i, streets[i].crossroad_from, streets[i].crossroad_to);
+        //kazdej ulice j
+        for (unsigned j=0; j<num_of_streets; ++j) {
+
+            // existuje nejaka ulica so zaciatkom 'z' a koncom 'k'
+            Clause *cl = create_new_clause(formula);
+            add_literal_to_clause(cl, true, i, streets[j].crossroad_from, streets[j].crossroad_to);
+        }
     }
 }
+
 
 
 // Tato funkce by mela do formule pridat klauzule predstavujici podminku 2)
@@ -40,18 +46,17 @@ void at_most_one_street_for_each_step(CNF* formula, unsigned num_of_crossroads, 
         for (unsigned z1 = 0; z1 < num_of_crossroads; ++z1) {
             for (unsigned z2 = 0; z2 < num_of_crossroads; ++z2) {
 
-                //kazdeho konca
+                //a kazdeho konca
                     for (unsigned k1=0; k1<num_of_crossroads; ++k1) {
                         for (unsigned k2 = 0; k2 < num_of_crossroads; ++k2) {
 
                             //existuje najviac 1 ulica na 1 krok
-                            //existujuca ulica =
-                            if (z1==z2 && (k1!=k2 || (z1 == k1 || z2 == k2)) && z1!=k2) {
+// TODO                         //existujuca ulica =
+// TODO                            if (z1!=z2 || k1!=k2 /*|| (z1 == k1 || z2 == k2)*/) {
                                 Clause *cl = create_new_clause(formula);
                                 add_literal_to_clause(cl, false, i, z1, k1);
                                 add_literal_to_clause(cl, false, i, z2, k2);
 
-                                //printf("%d, %d -- %d, %d\n", z1, k1, z2, k2);
                             }
                         }
                     }
@@ -83,7 +88,7 @@ void streets_connected(CNF* formula, unsigned num_of_crossroads, unsigned num_of
                     for (unsigned k2 = 0; k2 < num_of_crossroads; ++k2) {
 
                         //exituje konexia konca s nejakym zaciatkom inej ulice
-                        if (k1==z2) {
+                        if (k1==z2 && z1!=k2) {
                             Clause *cl = create_new_clause(formula);
                             add_literal_to_clause(cl, true, i, z1, k1);
                             add_literal_to_clause(cl, false, i+1, z2, k2);
